87b3740862afa35f1980302050a3d1a253f71ff3
AtharvRadhey <76675130+AtharvRadhey@users.noreply.github.com>
2021-06-25 14:44:26 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,37 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine,world;
+var Snow;
+var bg;
+
+function preload(){
+ bg=loadImage("snow1.jpg")
+}
+
+
+function setup() {
+  var canvas = createCanvas(900,400);
+  engine = Engine.create();
+  world = engine.world;
+
+if(frameCount%60==0){
+
+ Snow = new snowFlakes(random(100,800),50);
+ 
+}
+
+}
+
+function draw() {
+  background(bg); 
+ 
+Engine.update(engine);
+
+  Snow.display();
+
+
+  drawSprites();
+}
